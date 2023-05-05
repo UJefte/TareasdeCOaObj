edafff04e3b750991a5bc62327c4011d16bf009e
UJefte <113401613+UJefte@users.noreply.github.com>
2023-05-05 15:55:59 -0600

    Excepciones 2
    Videos

---
+++ ExepcionesDos/src/LeerArchivo.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerArchivo {
+    public  void leer(String ruta) throws IOException{
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(ruta));
+            String line="";
+            while ((line = reader.readLine()) != null){
+                System.out.println("Info" +  line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void metodoUno(String ruta) throws IOException{
+        this.metododos(ruta);
+    }
+    public void metododos(String ruta) throws IOException{
+        this.leer(ruta);
+    }
+}
